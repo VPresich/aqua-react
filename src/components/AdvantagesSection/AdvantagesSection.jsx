d95ca51a830cd662786c2422ef1e93492ebeb787
VPresich <valya.alexandrova.presich@gmail.com>
2025-01-26 02:39:24 +0100

    change css

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -10,16 +10,16 @@ const AdvantagesSection = () => {
         <Customers />
       </div>
 
-      <div className={css.advantagesGroupHabits}>
-        <ul className={css.advantagesList}>
-          <li className={css.advantagesHabit}>
+      <div className={css.group}>
+        <ul className={css.list}>
+          <li className={css.habit}>
             <div className={css.ellipse}></div>
             <p className={css.habit_1}>{t("advantagesSection.habit")}</p>
           </li>
-          <li className={css.advantagesHabit}>
+          <li className={css.habit}>
             <p className={css.habit_2}>{t("advantagesSection.statistics")}</p>
           </li>
-          <li className={css.advantagesHabit}>
+          <li className={css.habit}>
             <p className={css.habit_3}>{t("advantagesSection.setting")}</p>
           </li>
         </ul>
